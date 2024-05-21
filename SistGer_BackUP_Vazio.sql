2706bff4c21ae6b021d626a25d07012beee60566
Nelson-Almeida1 <168301294+Nelson-Almeida1@users.noreply.github.com>
2024-05-21 15:27:10 -0300

    Banco de Dados Vazio
    Banco de dados do Sistema Gerencial Casa Anselmo - vazio

---
+++ SistGer_BackUP_Vazio.sql
@@ -0,0 +1,185 @@
+-- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: db1
+-- ------------------------------------------------------
+-- Server version	10.11.6-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alimentos`
+--
+
+DROP TABLE IF EXISTS `alimentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alimentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(255) DEFAULT NULL,
+  `qtd` int(11) DEFAULT NULL,
+  `observacoes` varchar(500) DEFAULT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alimentos`
+--
+
+LOCK TABLES `alimentos` WRITE;
+/*!40000 ALTER TABLE `alimentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alimentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `diretoria`
+--
+
+DROP TABLE IF EXISTS `diretoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `diretoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL,
+  `observacoes` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diretoria`
+--
+
+LOCK TABLES `diretoria` WRITE;
+/*!40000 ALTER TABLE `diretoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `diretoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doadores`
+--
+
+DROP TABLE IF EXISTS `doadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `observacoes` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doadores`
+--
+
+LOCK TABLES `doadores` WRITE;
+/*!40000 ALTER TABLE `doadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `familias`
+--
+
+DROP TABLE IF EXISTS `familias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `familias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `observacoes` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `familias`
+--
+
+LOCK TABLES `familias` WRITE;
+/*!40000 ALTER TABLE `familias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `familias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movimentacao`
+--
+
+DROP TABLE IF EXISTS `movimentacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movimentacao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_familia` int(11) DEFAULT NULL,
+  `id_alimento` int(11) DEFAULT NULL,
+  `data_entrega` date DEFAULT NULL,
+  `qtd_entregue` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_familia` (`id_familia`),
+  KEY `id_alimento` (`id_alimento`),
+  CONSTRAINT `movimentacao_ibfk_1` FOREIGN KEY (`id_familia`) REFERENCES `familias` (`id`),
+  CONSTRAINT `movimentacao_ibfk_2` FOREIGN KEY (`id_alimento`) REFERENCES `alimentos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimentacao`
+--
+
+LOCK TABLES `movimentacao` WRITE;
+/*!40000 ALTER TABLE `movimentacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimentacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voluntarios`
+--
+
+DROP TABLE IF EXISTS `voluntarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `voluntarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `observacoes` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voluntarios`
+--
+
+LOCK TABLES `voluntarios` WRITE;
+/*!40000 ALTER TABLE `voluntarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `voluntarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-21 15:23:08
